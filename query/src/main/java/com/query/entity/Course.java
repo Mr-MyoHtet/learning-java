4b3a092efa78ce2c415a22ae5983cfad4a996c52
myohtet <52666491+Mr-MyoHtet@users.noreply.github.com>
2025-02-09 16:42:23 +0630

    added course entity

---
+++ query/src/main/java/com/query/entity/Course.java
@@ -0,0 +1,63 @@
+package com.query.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private int hours;
+	private int fees;
+
+	public Course(int id, String name, int hours, int fees) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.hours = hours;
+		this.fees = fees;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getHours() {
+		return hours;
+	}
+
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+
+	public int getFees() {
+		return fees;
+	}
+
+	public void setFees(int fees) {
+		this.fees = fees;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + ", hours=" + hours + ", fees=" + fees + "]";
+	}
+
+}
